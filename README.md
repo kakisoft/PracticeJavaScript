77028497e424615e1ec687d58b064f8312167c8b
kakisoft <kakisttab@gmail.com>
2018-03-07 23:52:26 +0900

    edit

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 # PracticeJavaScript
 JavaScript練習用  
 
-#### JavaScript
+### JavaScript
 https://kakisoft.github.io/PracticeJavaScript/practice_javascript.html
 
-#### jQuery
+### jQuery
 https://kakisoft.github.io/PracticeJavaScript/practice_jquery.html
 
-#### vue.js
+### vue.js
 https://kakisoft.github.io/PracticeJavaScript/vuejs2/practice_vuejs2.html
 
--- vuejs2/js/main.js
+++ vuejs2/js/main.js
@@ -6,10 +6,44 @@
   //UIを更新したらデータが更新され、データが更新されたらUIが更新される
 
   //vm：View Model
-  var vm = new Vue({
+  var vm01 = new Vue({
     el: '#app01',
     data: {
       name: 'kaki'
+    }
+  });
+
+  var vm02 = new Vue({
+    el: '.app02',
+    data: {
+      todos: [
+        'app02-1',
+        'app02-2',
+        'app02-3'
+      ]
+    }
+  });
+
+  var vm03 = new Vue({
+    el: '#app03',
+    data: {
+      newItem: '',
+      todos: [
+        'task 1',
+        'task 2',
+        'task 3'
+      ]
+    },
+    methods: {
+      //この記述が無いと、formがsubmitされて画面が遷移し、上手く行かない。（html側のディレクティブにて対処する方法もある）
+      // addItem: function(e) {
+      //   e.preventDefault();
+      //   this.todos.push(this.newItem);
+      // }
+      addItem: function() {
+        this.todos.push(this.newItem); //data内のデータは、thisでアクセスできる。
+        this.newItem = '';
+      }
     }
   });
 })();
