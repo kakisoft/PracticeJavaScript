0a578cd59a32ced4bc0f1906cc0b2f325d652ba0
kakisoft <kakisttab@gmail.com>
2018-03-10 10:42:53 +0900

    edit

--- vuejs2/js/practice-vuejs2-2.js
+++ vuejs2/js/practice-vuejs2-2.js
@@ -6,23 +6,47 @@
   //    部品を再利用するための仕組み。
   //      独自のタグを作成して、部品として配置できる。
   //==================================
-  var likeComponent1 = Vue.extend({
+  var myComponent1 = Vue.extend({
     //-----( テンプレートに書ける要素は１つのみ。)-----
-    template: '<button>Like</button>'
+    template: '<button>Component</button>'
     //-----( ↓の書き方だとNG。)-----
     // template: '<button>Like</button><button>Like</button>'
   });
 
-  var likeComponent2 = Vue.extend({
+  var myComponent2 = Vue.extend({
     //-----( 複数の要素を持たせる場合、何らかの親要素で囲む。)-----
     template: '<div><button>tmplete2-1</button><button>tmplete2-1</button></div>'
   });
 
-  var app = new Vue({
+  var appA = new Vue({
     el: '#app01',
     components: {
-      'like-component1': likeComponent1,
-      'like-component2': likeComponent2
+      'my-component1': myComponent1,
+      'my-component2': myComponent2
+    }
+  });
+
+  //==================================
+  // 
+  //==================================
+  var likeComponent = Vue.extend({
+    data: function() {
+      return {
+        count: 0
+      }
+    },
+    template: '<button @click="countUp">Like {{ count }}</button>',
+    methods: {
+      countUp: function() {
+        this.count++;
+      }
+    }
+  });
+
+  var appB = new Vue({
+    el: '#app01',
+    components: {
+      'like-component': likeComponent
     }
   });
 
