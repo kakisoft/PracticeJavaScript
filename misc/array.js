3a7906b36f04c91d0a6fd7c5fac2537e5b570441
kakisoft <kakisttab@gmail.com>
2018-09-21 13:28:57 +0900

    e

--- misc/array.js
+++ misc/array.js
@@ -2,6 +2,13 @@
 // IE 8以下は未対応
 
 //━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+var arr = ["1", "", "2", "", "4"];
+
+console.log(arr.filter(function (element) {
+  return element.trim().length > 0;
+}));
+
+//━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 var arr = [1, 2, 3, 4, 5];
 
 var isEven = function (element) {
