fccabc0f1dc673b56686a33ca7eeedcc26008c05
kakisoft <kakisttab@gmail.com>
2018-10-05 14:33:08 +0900

    e

--- misc/string01.js
+++ misc/string01.js
@@ -7,3 +7,7 @@ console.log(str1.padStart(2, '0'));
 
 //━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 
+const str1 = '5';
+
+console.log(str1.padStart(2, '0'));
+// expected output: "05"
--- sample01/sample01.js
+++
@@ -1,4 +0,0 @@
-const str1 = '5';
-
-console.log(str1.padStart(2, '0'));
-// expected output: "05"
