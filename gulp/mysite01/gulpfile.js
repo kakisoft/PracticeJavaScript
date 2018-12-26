52e01475166f70a6d4ce6e7003b4011dd42933c0
kakisoft <kakisttab@gmail.com>
2018-12-26 10:55:43 +0900

    e

--- gulp/mysite01/gulpfile.js
+++
@@ -1,7 +0,0 @@
-var gulp = require('gulp');
-
-gulp.task('hello', function() { // 'hello' という名前のタスク。『gulp hello』で実行。
-    console.log('hello world!');
-});
-
-gulp.task('default', ['hello']); // デフォルトで動作させるタスクを記述。ここでは、'hello' 
