a11a265edfb2276f37137a82a67fe4214833dffd
kakisoft <kakisttab@gmail.com>
2019-12-22 10:02:23 +0900

    c

--- 80_myParts/js/js_Array03_map_filter_reduce.js
+++ 80_myParts/js/js_Array03_map_filter_reduce.js
@@ -2,9 +2,9 @@
 // IE 8以下は未対応
 
 //━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-var arr = ["1", "", "2", "", "4"];
+var arr01 = ["1", "", "2", "", "4"];
 
-console.log(arr.filter(function (element) {
+console.log(arr01.filter(function (element) {
   return element.trim().length > 0;
 }));
 
@@ -12,25 +12,25 @@ console.log(arr.filter(function (element) {
 arr01 = arr01.filter( (element) => element.trim().length > 0 );
 
 //━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-var arr = [1, 2, 3, 4, 5];
+var arr02 = [1, 2, 3, 4, 5];
 
 var isEven = function (element) {
   // 偶数の場合はtrue、奇数の場合はfalse
   return element % 2 === 0;
 };
-console.log(arr.filter(isEven)); // [2, 4]
+console.log(arr02.filter(isEven)); // [2, 4]
 
 //━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-var arr = [1, 2, 3, 4, 5];
+var arr03 = [1, 2, 3, 4, 5];
 
-console.log(arr.filter(function (element) {
+console.log(arr03.filter(function (element) {
   return element % 2 === 0;
 })); // [2, 4]
 
 //━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 //オブジェクトの配列の場合
 
-var arr = [
+var arr04 = [
   {
     name: '一郎',
     age: 25
@@ -45,7 +45,7 @@ var arr = [
   }
 ];
 
-console.log(arr.filter(function (element) {
+console.log(arr04.filter(function (element) {
   return element.age >= 20;
 })); // [ {name: "一郎", age: 25}, {name: "二郎", age: 21} ]
 
@@ -54,15 +54,15 @@ console.log(arr.filter(function (element) {
 //【 reduce 】
 //  -> sum 
 
-const array1 = [1, 2, 3, 4];
+const array04 = [1, 2, 3, 4];
 const reducer = (accumulator, currentValue) => accumulator + currentValue;
 
 // 1 + 2 + 3 + 4
-console.log(array1.reduce(reducer));
+console.log(array04.reduce(reducer));
 // expected output: 10
 
 // 5 + 1 + 2 + 3 + 4
-console.log(array1.reduce(reducer, 5));
+console.log(array04.reduce(reducer, 5));
 // expected output: 15
 
 
