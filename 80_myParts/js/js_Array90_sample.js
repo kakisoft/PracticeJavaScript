461b98ac7004edbb6e2cc9b1afadb0bd9c2d72a7
kakisoft <kakisttab@gmail.com>
2020-03-11 08:23:56 +0900

    a

--- 80_myParts/js/js_Array90_sample.js
+++ 80_myParts/js/js_Array90_sample.js
@@ -2,20 +2,20 @@ function createInsertStatement(){
     var targetSyntaxElement = document.getElementById("target-syntax");
     var targetSyntaxContent = targetSyntaxElement.value;
     var targetSyntaxContentArray =  targetSyntaxContent.split(/\r?\n/g);
-  
+
     targetSyntaxContentArray.forEach((lineUnitElement)=>{
       var linerContentArray = [];
       var linerContentArray = lineUnitElement.split(/\t/g);
-  
-      
+
+
       linerContentArray.forEach(function(cellUnitElement) {
-        console.log(cellUnitElement)  
+        console.log(cellUnitElement)
       });
 
 
     });
   }
-  
+
 //━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 // 【 全て大文字 or 全て小文字 】
 
@@ -23,11 +23,11 @@ unitArray = unitArray.map((el) => el.toUpperCase());
 unitArray = unitArray.map((el) => el.toLowerCase());
 
 //━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-// 【 配列で指定した特定の文字を大文字化 】 
+// 【 配列で指定した特定の文字を大文字化 】
 
 unitArray = unitArray.map(
     function(el){
-      if(RESERVED_WORD_LIST.indexOf(el) >= 0){ 
+      if(RESERVED_WORD_LIST.indexOf(el) >= 0){
         return el.toLowerCase();
         // return el.toUpperCase();
       }else{
@@ -42,7 +42,7 @@ unitArray = unitArray.map(
   //-----< ToUppr or ToLower Reserved Words >-----
   if(resultType == RESULT_TYPE.TO_LOWER){
     unitArray = unitArray.map(function(el){
-        if(RESERVED_WORD_LIST.indexOf(el.toUpperCase()) >= 0){ 
+        if(RESERVED_WORD_LIST.indexOf(el.toUpperCase()) >= 0){
           return el.toLowerCase();
         }else{
           return el;
@@ -52,13 +52,13 @@ unitArray = unitArray.map(
 
   }else{
     unitArray = unitArray.map(function(el){
-        if(RESERVED_WORD_LIST.indexOf(el.toUpperCase()) >= 0){ 
+        if(RESERVED_WORD_LIST.indexOf(el.toUpperCase()) >= 0){
           return el.toUpperCase();
         }else{
           return el;
         }
       }
-    );    
+    );
   }
 
 //━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
