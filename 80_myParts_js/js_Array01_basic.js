621ae14740648f3d59f42682b498c63b13d478c1
kakisoft <kakisttab@gmail.com>
2020-05-16 17:39:21 +0900

    a

--- 80_myParts_js/js_Array01_basic.js
+++ 80_myParts_js/js_Array01_basic.js
@@ -19,8 +19,8 @@ Math.min(...array03);    //=> 2
 
 //-------------------------------
 const memberList = [
-  { name: 'Tom', age: 20 },
-  { name: 'Bob', age: 25 },
+  { name: 'Tom',  age: 20 },
+  { name: 'Bob',  age: 25 },
   { name: 'Cate', age: 23 },
   { name: 'John', age: 18 },
 ];
@@ -45,6 +45,72 @@ console.log(target.reduce((a,b)=>a.age>b.age?a:b).name);    //=> さん
 
 
 //━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+// 【 push  配列の末尾に 1 つ以上の要素を追加（戻り値として新しい配列の要素数を返す） 】
+
+var animals = ['pigs', 'goats', 'sheep'];
+
+console.log(animals.push('cows'));
+// expected output: 4
+
+console.log(animals);
+// expected output: Array ["pigs", "goats", "sheep", "cows"]
+
+//━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+// 【 pop  配列から最後の要素を取り除き、その要素を返します 】
+//（破壊的）
+
+var plants = ['broccoli', 'cauliflower', 'cabbage', 'kale', 'tomato'];
+
+console.log(plants.pop());
+// expected output: "tomato"
+
+console.log(plants);
+// expected output: Array ["broccoli", "cauliflower", "cabbage", "kale"]
+
+
+//━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+// 【 unshift -配列の先頭に値を追加します。 】
+
+var array = ['b', 'c'];
+
+array.unshift('a');
+console.log(array); // ['a', 'b', 'c']
+
+//━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+// 【 shift  配列から最初の要素を取り除き、その要素を返す。 】
+//（破壊的）
+
+var array1 = [1, 2, 3];
+
+var firstElement = array1.shift();
+
+console.log(array1);
+// expected output: Array [2, 3]
+
+console.log(firstElement);
+// expected output: 1
+
+//━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+// 【 reverse  反転 】
+
+var array1 = ['one', 'two', 'three'];
+console.log('array1: ', array1);
+// expected output: Array ['one', 'two', 'three']
+
+var reversed = array1.reverse(); 
+console.log('reversed: ', reversed);
+// expected output: Array ['three', 'two', 'one']
+
+//━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+// 【 特定の要素を削除 】
+
+var startWord = "SELECT";
+var endWord = ["FROM","WHERE","GROUP BY","SELECT","ORDER BY"]
+
+endWord = endWord.filter((el)=> el !== startWord);
+
+//━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+
 
 
 
